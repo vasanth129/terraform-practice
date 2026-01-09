02412c2447c30e4b7c5dbf3de7a5f61afa177af3
vasanth <vasanthmudhiraj129@gmail.com>
2026-01-09 15:48:40 +0530

    day-3 statefile updated

---
+++ day-3-statefile/terraform.tfvars
@@ -0,0 +1,7 @@
+ami_id = "ami-0ced6a024bb18ff2e"
+key_name = "kabali_access"
+instance_type = "t2.micro"
+
+# region : ap-south-1
+# amazon linux -> ami-0ced6a024bb18ff2e
+# redhat linux -> ami-01ca13db604661046
---
+++ day-3-statefile/variables.tf
@@ -0,0 +1,14 @@
+variable "instance_type" {
+    type = string
+    default = ""
+}
+
+variable "ami_id" {
+    type = string
+    default = ""
+}
+
+variable "key_name" {
+    type = string
+    default = ""
+}
