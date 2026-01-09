11ff0f8d8e1e465883d66817416e95f53bf4cc36
vasanth <vasanthmudhiraj129@gmail.com>
2026-01-10 01:42:45 +0530

    some small changes

--- day-4-remote-state/main.tf
+++ day-4-remote-state/main.tf
@@ -2,6 +2,9 @@ resource "aws_instance" "name" {
     ami = var.ami_id
     key_name = var.key_name
     instance_type = var.instance_type
+    tags = {
+        "Name" = "Dev"
+    }
 }
 
 resource "aws_s3_bucket" "name" {
