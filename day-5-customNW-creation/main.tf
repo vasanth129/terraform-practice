f662f7a3751d971b03e291ce3ec2ea18eae13db4
vasanth <vasanthmudhiraj129@gmail.com>
2026-01-11 05:44:13 +0530

    added NAT configurations

--- day-5-customNW-creation/main.tf
+++ day-5-customNW-creation/main.tf
@@ -77,3 +77,60 @@ resource "aws_instance" "dev" {
     Name = "dev-instance"
   }
 }
+
+# creating private subnet, NAT GW, private RT.
+resource "aws_subnet" "private_sub" {
+  vpc_id = aws_vpc.dev.id
+  cidr_block = "192.168.0.16/28"
+  availability_zone = "ap-south-1b"
+}
+
+resource "aws_eip" "dev" {
+  domain =  "vpc"
+
+  tags = {
+    Name = "dev-eip"
+  }
+
+  depends_on = [ aws_internet_gateway.dev ]
+}
+
+resource "aws_nat_gateway" "NATGW" {
+  subnet_id = aws_subnet.dev.id
+  allocation_id = aws_eip.dev.id
+
+  tags = {
+    Name = "dev-NAT"
+  }
+}
+
+resource "aws_route_table" "private-RT" {
+  vpc_id = aws_vpc.dev.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.NATGW.id
+  }
+}
+
+resource "aws_route_table_association" "priavte-sub-assoc" {
+  subnet_id = aws_subnet.private_sub.id
+  route_table_id = aws_route_table.private-RT.id
+}
+
+# create private instance and test
+
+resource "aws_instance" "private_instance" {
+  subnet_id = aws_subnet.private_sub.id
+  instance_type = "t2.micro"
+  ami = "ami-01ca13db604661046"
+  key_name = "kabali_access"
+  vpc_security_group_ids = [aws_security_group.dev.id]
+
+  tags = {
+    Name = "dev-private-instance"
+  }
+  
+}
+
+
