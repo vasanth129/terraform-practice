aec13eaea973024d3c37fc2549576a93cc0ee268
vasanth <vasanthmudhiraj129@gmail.com>
2026-01-12 16:39:45 +0530

    day-6 generate-config-out using import block

--- day-6-import/main.tf
+++ day-6-import/main.tf
@@ -1,7 +1,14 @@
-resource "aws_instance" "web" {
-  ami = "ami-01ca13db604661046"
-  instance_type = "t2.micro"
+import {
+  to = aws_instance.web
+  id = "i-0bb69b7ab1adcd4a9"
+}
 
+resource "aws_instance" "web" {
+  ami                                  = "ami-01ca13db604661046"
+  instance_type                        = "t2.micro"
+  key_name                             = "kabali_access"
+  security_groups                      = ["pub-server-sg"]
+  subnet_id                            = "subnet-0fea1a116522d79d9"
   tags = {
     Name = "web-instance"
   }
